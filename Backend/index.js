fcc0a7680fb78634e2823ee8acbae296d1d790d6
Ayushi Bansiwal <ayushibansiwal2006@hotmail.com>
2026-07-08 22:47:06 +0530

    Refactor express app initialization and middleware setup

--- Backend/index.js
+++ Backend/index.js
@@ -14,20 +14,6 @@ import apiRoutes from "./Routes/apiRoutes.js";
 
 dotenv.config();
 
-const app = express();
-const PORT = process.env.PORT || 8000;
-const MONGODB_URL = process.env.MONGODB_URL;
-
-async function main() {
-    await mongoose.connect(MONGODB_URL);
-    console.log("✅ Database connected successfully.");
-}
-
-// Global Middleware
-const allowedOrigins = [
-  "https://zerodaclone-frontend.onrender.com/"
-];
-
 app.use(cors({
   origin: function (origin, callback) {
     if (!origin) return callback(null, true);
@@ -44,6 +30,21 @@ app.use(cors({
 }));
 
 app.options('*', cors());
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+const MONGODB_URL = process.env.MONGODB_URL;
+
+async function main() {
+    await mongoose.connect(MONGODB_URL);
+    console.log("✅ Database connected successfully.");
+}
+
+// Global Middleware
+const allowedOrigins = [
+  "https://zerodaclone-frontend.onrender.com/"
+];
+
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(session({
